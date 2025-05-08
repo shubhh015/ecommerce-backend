2bb65c5f51e1768ceeeb9ee095ee7ed054c6f651
shubhh015 <shubhashishhhh@gmail.com>
2025-05-08 10:27:21 +0530

    changes in model

--- models/Order.js
+++ models/Order.js
@@ -6,23 +6,6 @@ const orderItemSchema = new mongoose.Schema({
         ref: "Product",
         required: true,
     },
-    image: {
-        url: {
-            type: String,
-            required: true,
-            validate: {
-                validator: function (v) {
-                    return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(v);
-                },
-                message: (props) => `${props.value} is not a valid URL!`,
-            },
-        },
-        alt: {
-            type: String,
-            required: true,
-            default: "Product image",
-        },
-    },
     name: { type: String, required: true },
     quantity: { type: Number, required: true },
     price: { type: Number, required: true },
