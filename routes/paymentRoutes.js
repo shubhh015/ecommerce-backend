a65176c52079ed0bf8098e6825fc0083b6afa7de
shubhh015 <shubhashishhhh@gmail.com>
2025-05-13 14:53:12 +0530

    removed middleware

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,9 @@
 // routes/paymentRoutes.js
 import express from "express";
 import { createOrder } from "../controllers/paymentController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/orders", authMiddleware, createOrder);
+router.post("/orders", createOrder);
 
 export default router;
